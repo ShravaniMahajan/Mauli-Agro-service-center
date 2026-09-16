a168dd80cc4faaa9135ad910b94bde1a94f74303
Shravani Bharat Mahajan <2460001@ritindia.edu>
2026-09-16 20:07:44 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register"; // ✅ ADD THIS
-import Dashboard from "./pages/Dashboard";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        
-        <Route path="/register" element={<Register />} /> {/* ✅ NEW */}
-        
+        <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
